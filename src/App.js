0879af07e107e2768187a69fc65514a7c7a62fa8
namchtq238 <92732447+namchtq238@users.noreply.github.com>
2022-12-08 05:40:34 +0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -13,392 +13,35 @@ export default function App() {
 }
 
 const FormCustom = () => {
-  const data = [
-    {
-        "id": null,
-        "team1": {
-            "id": 1,
-            "teamName": "Arsenal"
-        },
-        "team2": {
-            "id": 2,
-            "teamName": "Liverpool"
-        },
-        "isDerby": false
-    },
-    {
-        "id": null,
-        "team1": {
-            "id": 3,
-            "teamName": "Chelsea"
-        },
-        "team2": {
-            "id": 4,
-            "teamName": "Newcatsle United"
-        },
-        "isDerby": false
-    },
-    {
-        "id": null,
-        "team1": {
-            "id": 5,
-            "teamName": "Manchester United"
-        },
-        "team2": {
-            "id": 6,
-            "teamName": "Manchester City"
-        },
-        "isDerby": false
-    },
-    {
-        "id": null,
-        "team1": {
-            "id": 1,
-            "teamName": "Arsenal"
-        },
-        "team2": {
-            "id": 2,
-            "teamName": "Liverpool"
-        },
-        "isDerby": false
-    },
-    {
-        "id": null,
-        "team1": {
-            "id": 3,
-            "teamName": "Chelsea"
-        },
-        "team2": {
-            "id": 4,
-            "teamName": "Newcatsle United"
-        },
-        "isDerby": false
-    },
-    {
-        "id": null,
-        "team1": {
-            "id": 5,
-            "teamName": "Manchester United"
-        },
-        "team2": {
-            "id": 6,
-            "teamName": "Manchester City"
-        },
-        "isDerby": false
-    },
-    {
-        "id": null,
-        "team1": {
-            "id": 1,
-            "teamName": "Arsenal"
-        },
-        "team2": {
-            "id": 2,
-            "teamName": "Liverpool"
-        },
-        "isDerby": false
-    },
-    {
-        "id": null,
-        "team1": {
-            "id": 3,
-            "teamName": "Chelsea"
-        },
-        "team2": {
-            "id": 4,
-            "teamName": "Newcatsle United"
-        },
-        "isDerby": false
-    },
-    {
-        "id": null,
-        "team1": {
-            "id": 5,
-            "teamName": "Manchester United"
-        },
-        "team2": {
-            "id": 6,
-            "teamName": "Manchester City"
-        },
-        "isDerby": false
-    },
-    {
-        "id": null,
-        "team1": {
-            "id": 1,
-            "teamName": "Arsenal"
-        },
-        "team2": {
-            "id": 2,
-            "teamName": "Liverpool"
-        },
-        "isDerby": false
-    },
-    {
-        "id": null,
-        "team1": {
-            "id": 3,
-            "teamName": "Chelsea"
-        },
-        "team2": {
-            "id": 4,
-            "teamName": "Newcatsle United"
-        },
-        "isDerby": false
-    },
-    {
-        "id": null,
-        "team1": {
-            "id": 5,
-            "teamName": "Manchester United"
-        },
-        "team2": {
-            "id": 6,
-            "teamName": "Manchester City"
-        },
-        "isDerby": false
-    },
-    {
-        "id": null,
-        "team1": {
-            "id": 1,
-            "teamName": "Arsenal"
-        },
-        "team2": {
-            "id": 2,
-            "teamName": "Liverpool"
-        },
-        "isDerby": false
-    },
-    {
-        "id": null,
-        "team1": {
-            "id": 3,
-            "teamName": "Chelsea"
-        },
-        "team2": {
-            "id": 4,
-            "teamName": "Newcatsle United"
-        },
-        "isDerby": false
-    },
-    {
-        "id": null,
-        "team1": {
-            "id": 5,
-            "teamName": "Manchester United"
-        },
-        "team2": {
-            "id": 6,
-            "teamName": "Manchester City"
-        },
-        "isDerby": false
-    },
-    {
-        "id": null,
-        "team1": {
-            "id": 1,
-            "teamName": "Arsenal"
-        },
-        "team2": {
-            "id": 2,
-            "teamName": "Liverpool"
-        },
-        "isDerby": false
-    },
-    {
-        "id": null,
-        "team1": {
-            "id": 3,
-            "teamName": "Chelsea"
-        },
-        "team2": {
-            "id": 4,
-            "teamName": "Newcatsle United"
-        },
-        "isDerby": false
-    },
-    {
-        "id": null,
-        "team1": {
-            "id": 5,
-            "teamName": "Manchester United"
-        },
-        "team2": {
-            "id": 6,
-            "teamName": "Manchester City"
-        },
-        "isDerby": false
-    },
-    {
-        "id": null,
-        "team1": {
-            "id": 1,
-            "teamName": "Arsenal"
-        },
-        "team2": {
-            "id": 2,
-            "teamName": "Liverpool"
-        },
-        "isDerby": false
-    },
-    {
-        "id": null,
-        "team1": {
-            "id": 3,
-            "teamName": "Chelsea"
-        },
-        "team2": {
-            "id": 4,
-            "teamName": "Newcatsle United"
-        },
-        "isDerby": false
-    },
-    {
-        "id": null,
-        "team1": {
-            "id": 5,
-            "teamName": "Manchester United"
-        },
-        "team2": {
-            "id": 6,
-            "teamName": "Manchester City"
-        },
-        "isDerby": false
-    },
-    {
-        "id": null,
-        "team1": {
-            "id": 1,
-            "teamName": "Arsenal"
-        },
-        "team2": {
-            "id": 2,
-            "teamName": "Liverpool"
-        },
-        "isDerby": false
-    },
-    {
-        "id": null,
-        "team1": {
-            "id": 3,
-            "teamName": "Chelsea"
-        },
-        "team2": {
-            "id": 4,
-            "teamName": "Newcatsle United"
-        },
-        "isDerby": false
-    },
-    {
-        "id": null,
-        "team1": {
-            "id": 5,
-            "teamName": "Manchester United"
-        },
-        "team2": {
-            "id": 6,
-            "teamName": "Manchester City"
-        },
-        "isDerby": false
-    },
-    {
-        "id": null,
-        "team1": {
-            "id": 1,
-            "teamName": "Arsenal"
-        },
-        "team2": {
-            "id": 2,
-            "teamName": "Liverpool"
-        },
-        "isDerby": false
-    },
-    {
-        "id": null,
-        "team1": {
-            "id": 3,
-            "teamName": "Chelsea"
-        },
-        "team2": {
-            "id": 4,
-            "teamName": "Newcatsle United"
-        },
-        "isDerby": false
-    },
-    {
-        "id": null,
-        "team1": {
-            "id": 5,
-            "teamName": "Manchester United"
-        },
-        "team2": {
-            "id": 6,
-            "teamName": "Manchester City"
-        },
-        "isDerby": false
-    },
-    {
-        "id": null,
-        "team1": {
-            "id": 1,
-            "teamName": "Arsenal"
-        },
-        "team2": {
-            "id": 2,
-            "teamName": "Liverpool"
-        },
-        "isDerby": false
-    },
-    {
-        "id": null,
-        "team1": {
-            "id": 3,
-            "teamName": "Chelsea"
-        },
-        "team2": {
-            "id": 4,
-            "teamName": "Newcatsle United"
-        },
-        "isDerby": false
-    },
-    {
-        "id": null,
-        "team1": {
-            "id": 5,
-            "teamName": "Manchester United"
-        },
-        "team2": {
-            "id": 6,
-            "teamName": "Manchester City"
-        },
-        "isDerby": false
-    }
-]
+    function timeout(milliseconds, promise) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            reject(new Error("timeout exceeded"))
+          }, milliseconds)
+          promise.then(resolve, reject)
+        })
+      }
+            
   const [dataSource, setDataSource] = useState([]);
   const [form] = Form.useForm();
   const onFinish = (values) => {
-    const names = Object.values(values).map((x) => x);
-    console.log('Success:', JSON.stringify(names));
 
-    fetch('localhost:8090/abc', {
-      body: JSON.stringify(names),
-      method: 'POST',
-    })
-      .then((x) => x.json())
+    timeout(60000, fetch('localhost:8080/result'))
+    .then((x) => x.json())
       .then((x) => {
         console.log(x);
-        const dataSource = data
-        console.log(data)
+        const dataSource = x;
+        console.log(data);
         setDataSource(dataSource);
         form.resetFields();
       })
-      .catch((err) => {console.log(err)
-        console.log(data)
+      .catch((err) => {
+        console.log(err);
+        console.log(data);
         const dataSource = data;
         setDataSource(dataSource);
         form.resetFields();
-});
+      });
   };
 
   const onFinishFailed = (errorInfo) => {
@@ -406,15 +49,15 @@ const FormCustom = () => {
   };
   return (
     <>
-        <Form
-          name="basic"
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          autoComplete="off"
-          form={form}
-        >
-          {/* {Array.from({ length: 6 }).map((_, index) => (
+      <Form
+        name="basic"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+        form={form}
+      >
+        {/* {Array.from({ length: 6 }).map((_, index) => (
             <Form.Item
               label={`Name ${index + 1}`}
               name={`name_${index}`}
@@ -423,13 +66,13 @@ const FormCustom = () => {
               <Input />
             </Form.Item>
           ))} */}
-          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-            <Button type="primary" htmlType="submit">
-              Process
-            </Button>
-          </Form.Item>
-        </Form>
-      
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Process
+          </Button>
+        </Form.Item>
+      </Form>
+
       <TableCustom dataSource={dataSource} />
     </>
   );
@@ -452,19 +95,21 @@ const TableCustom = (props) => {
       render: (_, record) => {
         return record.team2.teamName;
       },
-
     },
     {
       title: 'Derby Match',
       dataIndex: 'isDerby',
       key: 'isDerby',
-      render : (text) => String(text),
+      render: (text) => String(text),
     },
   ];
 
-  return <Table dataSource={props.dataSource} 
-  columns={columns} 
-  pagination={false}
-  rowClassName={(_r, i) => (i % 6 <3 ? 'active-row' : '')}
-  />;
+  return (
+    <Table
+      dataSource={props.dataSource}
+      columns={columns}
+      pagination={false}
+      rowClassName={(_r, i) => (i % 6 < 3 ? 'active-row' : '')}
+    />
+  );
 };
